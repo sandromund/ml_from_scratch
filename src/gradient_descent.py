1c05d8ecdc892c9b26885a625468be623c17b996
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-26 18:19:34 +0100

    Update gradient_descent.py

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -1,10 +1,8 @@
-
-
 def gradient_descent(X, Y, learning_rate=0.3, m=0, b=0, iterations=5):
     for i in range(iterations):
-        b_gradient = - (2 / len(X)) * sum([ Y[i] - ( m * X[i] + b)
+        b_gradient = - (2 / len(X)) * sum([Y[i] - (m * X[i] + b)
                                            for i in range(len(X))])
-        m_gradient = - (2 / len(X)) * sum([ X[i] * (Y[i] - ( m * X[i] + b))
+        m_gradient = - (2 / len(X)) * sum([X[i] * (Y[i] - (m * X[i] + b))
                                            for i in range(len(X))])
         b = b - (learning_rate * b_gradient)
         m = m - (learning_rate * m_gradient)
@@ -12,9 +10,8 @@ def gradient_descent(X, Y, learning_rate=0.3, m=0, b=0, iterations=5):
 
 
 if __name__ == '__main__':
-
     from sklearn.datasets import make_regression
-    from src.misc import mean_square_error
+    from misc import mean_square_error
     import matplotlib.pyplot as plt
     import pandas as pd
 
